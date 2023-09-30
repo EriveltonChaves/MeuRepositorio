438cb288e1ea0a23163046b14ed2dc345b87b47f
EriveltonChaves <eriveltonecs@gmail.com>
2023-09-30 22:36:25 +0100

    Add files via upload

---
+++ ImpressaoDeVariaveis/Variaveis/script.js
@@ -0,0 +1,5 @@
+let nome = 'Lisboa'
+let conselhos = 18;
+let linda = 'verdade'
+
+console.log('Eu moro em ' + nome + '. Ela tem ' + conselhos + ' conselhos. Eu acho ela muito bonita de ' + linda + '.')
